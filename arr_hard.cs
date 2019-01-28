c9b0b77887370c4e9fc2bc432639505f7d2ef64a
Sabotage84 <sabotage84@mail.ru>
2019-01-28 15:22:20 +0300

    ARR hard done

--- arr_hard.cs
+++ arr_hard.cs
@@ -12,33 +12,31 @@ using System.Text.RegularExpressions;
 using System.Text;
 using System;
 
-class Solution
-{
+class Solution {
 
-    // Complete the arrayManipulation function below. 
-    static long arrayManipulation(int n, int[][] queries)
-    {
-        int[] ARR = new int[n];
-        long max = 0;
-        for (int i = 0; i < queries.GetLength(0); i++)
-        {
-            for (int j = queries[i][0] - 1; j < queries[i][1]; j++)
-            {
-                ARR[j] += queries[i][2];
-                if (max < ARR[j])
-                    max = ARR[j];
-                Console.WriteLine(max.ToString());
+    // Complete the arrayManipulation function below.
+    static long arrayManipulation(int n, int[][] queries) {
+        long[] ARR = new long[n+1];
+        long max=0;
+        for (int i=0; i<queries.GetLength(0); i++){
+            ARR[queries[i][0]-1]+=queries[i][2];
+            ARR[queries[i][1]]-=queries[i][2];
             }
-        }
+            long t=ARR[0];
+            max=ARR[0];
+            for (int i=1; i<ARR.Length; i++){
+                ARR[i]+=ARR[i-1];
+                if (ARR[i]>max)
+                    max=ARR[i];
+            }
+        
         return max;
     }
 
-    static void Main(string[] args)
-    {
-        string[] arStr = File.ReadAllLines("arr.txt");
+    static void Main(string[] args) {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
 
-
-        string[] nm = arStr[0].Split(' ');
+        string[] nm = Console.ReadLine().Split(' ');
 
         int n = Convert.ToInt32(nm[0]);
 
@@ -46,15 +44,15 @@ class Solution
 
         int[][] queries = new int[m][];
 
-        for (int i = 0; i < m; i++)
-        {
-            queries[i] = Array.ConvertAll(arStr[i+1].Split(' '), queriesTemp => Convert.ToInt32(queriesTemp));
+        for (int i = 0; i < m; i++) {
+            queries[i] = Array.ConvertAll(Console.ReadLine().Split(' '), queriesTemp => Convert.ToInt32(queriesTemp));
         }
 
         long result = arrayManipulation(n, queries);
 
-        Console.WriteLine(result);
+        textWriter.WriteLine(result);
 
-        Console.ReadKey();
+        textWriter.Flush();
+        textWriter.Close();
     }
 }
