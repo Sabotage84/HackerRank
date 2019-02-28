f4ed7d3c5391ed48cbccbcc832232138f184f77a
sabotage84@mail.ru <sabotage84@mail.ru>
2019-02-28 17:54:25 +0300

    ADD triple5 not work

--- ConsoleApp13/ConsoleApp13/Program.cs
+++ ConsoleApp13/ConsoleApp13/Program.cs
@@ -10,15 +10,18 @@ namespace ConsoleApp13
     {
         static void Main(string[] args)
         {
-            //long r = 3;
-            //long[] lon = new long[6] { 1, 3, 9, 9, 27, 81 };
+            long r = 3;
+            long[] lon = new long[6] { 1, 3, 9, 9, 27, 81 };
 
-            long r = 1;
-            long[] lon = new long[100];
-            for (int i = 0; i < lon.Length; i++)
-            {
-                lon[i] = 1;
-            }
+            //long r = 2;
+            //long[] lon = new long[4] { 1, 2, 2, 4 };
+
+            //long r = 1;
+            //long[] lon = new long[100];
+            //for (int i = 0; i < lon.Length; i++)
+            //{
+            //    lon[i] = 1;
+            //}
 
             long res = 0;
             res = foo(lon.ToList(), r);
@@ -31,38 +34,33 @@ namespace ConsoleApp13
         private static long foo(List<long> arr, long r)
         {
             long res = 0;
-            //Dictionary<int, long> firstLevel = new Dictionary<int, long>();
+            
             Dictionary<long, int> secondLevel = new Dictionary<long, int>();
             Dictionary<long, int> thirdLevel = new Dictionary<long, int>();
-            int i=1, j=1;
-             arr.Reverse();
+           
+            arr.Reverse();
             foreach (var item in arr)
             {
-               if(secondLevel.Keys.Contains(item*r) && i>2)
+                if (thirdLevel.ContainsKey(item * r))
+                    res += thirdLevel[item*r];
+
+
+                if (secondLevel.Keys.Contains(item * r))
                 {
-                    if (thirdLevel.Keys.Contains(item*r*r))
-                    {
-                        res += secondLevel[item * r] * thirdLevel[item * r * r];
-                    }
+                    
+                    if (thirdLevel.Keys.Contains(item))
+                        thirdLevel[item]=secondLevel[item*r];
+                    else
+                        thirdLevel.Add(item, 1);
+                    secondLevel[item*r]++;
                 }
 
+                
                 if (secondLevel.Keys.Contains(item))
-                {
-                    if (i > 2)
-                        secondLevel[item]++;
-                }
+                    secondLevel[item]++;
                 else
                     secondLevel.Add(item, 1);
-
-                if (thirdLevel.Keys.Contains(item))
-                {
-                    if (i > 3)
-                        thirdLevel[item]++;
-                }
-                else
-                    thirdLevel.Add(item, 1);
-                i++;
-                j++;
+                
             }
 
             return res;
