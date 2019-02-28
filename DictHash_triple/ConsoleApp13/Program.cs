4375826c51643f9c0527b55a964a5be666d35cc9
IvanZ <sabotage84@mail.ru>
2019-02-28 22:31:06 +0300

    ADD triple, done

--- DictHash_triple/ConsoleApp13/Program.cs
+++ DictHash_triple/ConsoleApp13/Program.cs
@@ -10,19 +10,21 @@ namespace ConsoleApp13
     {
         static void Main(string[] args)
         {
-            long r = 3;
-            long[] lon = new long[6] { 1, 3, 9, 9, 27, 81 };
+            //long r = 3;
+            //long[] lon = new long[6] { 1, 3, 9, 9, 27, 81 };
 
             //long r = 2;
             //long[] lon = new long[4] { 1, 2, 2, 4 };
 
-            //long r = 1;
-            //long[] lon = new long[100];
-            //for (int i = 0; i < lon.Length; i++)
-            //{
-            //    lon[i] = 1;
-            //}
+            long r = 1;
 
+            //long[] lon = new long[5] { 1, 1, 1, 1, 1 };
+            long[] lon = new long[100000];
+            for (int i = 0; i < lon.Length; i++)
+            {
+                lon[i] = 1;
+            }
+            long realAnswer = 161700;
             long res = 0;
             res = foo(lon.ToList(), r);
             Console.WriteLine(res);
@@ -35,8 +37,8 @@ namespace ConsoleApp13
         {
             long res = 0;
             
-            Dictionary<long, int> secondLevel = new Dictionary<long, int>();
-            Dictionary<long, int> thirdLevel = new Dictionary<long, int>();
+            Dictionary<long, long> secondLevel = new Dictionary<long, long>();
+            Dictionary<long, long> thirdLevel = new Dictionary<long, long>();
            
             arr.Reverse();
             foreach (var item in arr)
@@ -47,12 +49,12 @@ namespace ConsoleApp13
 
                 if (secondLevel.Keys.Contains(item * r))
                 {
-                    
+                    //secondLevel[item*r]++;
                     if (thirdLevel.Keys.Contains(item))
-                        thirdLevel[item]=secondLevel[item*r];
+                        thirdLevel[item]+=secondLevel[item*r];
                     else
-                        thirdLevel.Add(item, 1);
-                    secondLevel[item*r]++;
+                        thirdLevel.Add(item, secondLevel[item*r]);
+                    
                 }
 
                 
