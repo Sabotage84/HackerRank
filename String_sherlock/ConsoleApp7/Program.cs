4326c8f5337e604d1bd3f4e4319e1bd80ba6b1d3
IvanZ <sabotage84@mail.ru>
2019-03-23 23:11:49 +0300

    ADD string_sher

---
+++ String_sherlock/ConsoleApp7/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1 = "aaabbbbcccddd";
+            string str2 = "aaabcccddd";
+            string str3 = "aabbccddeefghi";
+            Console.WriteLine(isValid(str1));
+            Console.WriteLine(isValid(str2));
+            Console.WriteLine(isValid(str3));
+
+            Console.ReadKey();
+        }
+
+        static string isValid(string s)
+        {
+           
+            Dictionary<char, int> dic = new Dictionary<char, int>();
+            Dictionary<int, int> dicInt = new Dictionary<int, int>();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (dic.ContainsKey(s[i]))
+                    dic[s[i]]++;
+                else
+                    dic.Add(s[i], 1);
+            }
+          
+            foreach (var item in dic)
+            {
+                if (dicInt.ContainsKey(item.Value))
+                    dicInt[item.Value]++;
+                else
+                    dicInt.Add(item.Value, 1);
+            }
+            if (dicInt.Count > 2)
+                return "NO";
+            else if (dicInt.Count == 1)
+                return "YES";
+            else
+            {
+                if ((dicInt.Values.ElementAt(0)) == 1 && (dicInt.Keys.ElementAt(0)) == 1)
+                    return "YES";
+                if ((dicInt.Values.ElementAt(1)) == 1 && (dicInt.Keys.ElementAt(1)) == 1)
+                    return "YES";
+                if ((dicInt.Values.ElementAt(0) )==1 && (Math.Abs(dicInt.Keys.ElementAt(0)-dicInt.Keys.ElementAt(1)))==1)
+                {
+                    return "YES";
+                }
+                else if (dicInt.Values.ElementAt(1)==1  && (Math.Abs(dicInt.Keys.ElementAt(0) - dicInt.Keys.ElementAt(1))) == 1)
+                {
+                    return "YES";
+                }
+               //else if(dicInt.Keys.ElementAt(0)==Math.Abs(dicInt.) {
+
+               // }
+
+
+                else
+                    return "NO";
+            }
+
+
+        }
+
+    }
+}
---
+++ String_sherlock/ConsoleApp7/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("ConsoleApp7")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConsoleApp7")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("c1257a9a-c886-40a3-9f71-7f6b7d8a89ca")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
