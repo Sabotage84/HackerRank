f6538e84937d99613d36e7f6a29fae5a2b6e0df7
IvanZ <sabotage84@mail.ru>
2019-03-31 23:23:47 +0300

    ADD strings common child, proj and tests

---
+++ Strings_common_child/ConsoleApp8/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        public static int commonChild(string s1, string s2)
+        {
+            int res = 0;
+
+
+            return res;
+        }
+    }
+}
---
+++ Strings_common_child/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApp8;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(3,Program.commonChild("ABCD", "ABDC"));
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.AreEqual(2, Program.commonChild("HARRY", "SALLY"));
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Assert.AreEqual(3, Program.commonChild("SHINCHAN", "NOHARAAA"));
+        }
+    }
+}
