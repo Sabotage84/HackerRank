2d00cbbf2bbc68c281645b9d975d9cc9a2f2b235
sabotage84@mail.ru <sabotage84@mail.ru>
2019-04-03 14:27:23 +0300

    Strings CommonChild req work but slow

--- Strings_common_child/ConsoleApp8/Program.cs
+++ Strings_common_child/ConsoleApp8/Program.cs
@@ -10,82 +10,49 @@ namespace ConsoleApp8
     {
         static void Main(string[] args)
         {
-            commonChild("WEWOUCUIDGCGTRMEZEPXZFEJWISRSBBSYXAYDFEJJDLEBVHHKS", "FDAGCXGKCTKWNECHMRXZWMLRYUCOCZHJRRJBOAJOQJZZVUYXIC");
+            //commonChild("WEWOUCUIDGCGTRMEZEPXZFEJWISRSBBSYXAYDFEJJDLEBVHHKS", "FDAGCXGKCTKWNECHMRXZWMLRYUCOCZHJRRJBOAJOQJZZVUYXIC");
+            commonChild("SHINCHAN", "NOHARAAA");
             Console.ReadKey();
         }
         public static int commonChild(string s1, string s2)
         {
             int res = 0;
 
-            res = CommonChildReq(s1, s2,0);
-            //string tempres = "";
-            //List<char> chars= s2.ToList();
-            //for (int j = 0; j < s1.Length; j++)
-            //{
-            //    for (int i = j; i < s1.Length; i++)
-            //    {
-            //        if (chars.Contains(s1[i]))
-            //        {
-            //            tempres += s1[i];
-            //            chars.RemoveRange(0, chars.FindIndex((x => s1[i] == x)) + 1);
+            res = CommonChildReq3(s1, s2, 0);
 
-            //        }
-            //    }
-            //    if(tempres.Length>res)
-            //        res = tempres.Length;
-            //    Console.WriteLine(tempres);
-            //    Console.WriteLine(tempres.Length);
-            //    tempres = "";
-            //    chars= s2.ToList();
-            //}
-            
             return res;
         }
 
-        private static int CommonChildReq(string s1, string s2, int start)
+        private static int CommonChildReq3(string s1, string s2, int start)
         {
-            string res = "";
-            int resInt = 0;
-            List<char> chars = s2.ToList();
-            for (int i = start; i < s1.Length; i++)
+            int res = 0;
+            int tempRes = 0;
+            List<char> alredyDone = new List<char>();
+            List<Char> tempCharList = s2.ToList();
+            for (int i = start; (i < s1.Length && res<(s1.Length-i)); i++)
             {
-                if (chars.Contains(s1[i]))
+                if (!alredyDone.Contains(s1[i]))
                 {
-                    Console.WriteLine(s1[i]);
-                    chars.RemoveRange(0, chars.FindIndex((x => s1[i] == x)) + 1);
-                    res += s1[i];
-                    res+= CommonChildReq2(s1, chars, i+1);
+                    alredyDone.Add(s1[i]);
+                    if (tempCharList.Contains(s1[i]))
+                    {
+                        
+                        tempRes++;
+                        int temp = tempCharList.FindIndex((x => s1[i] == x)) + 1;
+                        if(s2.Length - temp>res)
+                            tempRes += CommonChildReq3(s1, s2.Substring(temp, s2.Length - temp), i + 1);
+                    }
+                    
+                    if (tempRes > res)
+                        res = tempRes;
+                    tempRes = 0;
+                    tempCharList = s2.ToList();
                 }
-                Console.WriteLine();
-                Console.WriteLine(res);
-                Console.WriteLine(res.Length);
-                if (res.Length > resInt)
-                    resInt = res.Length;
-                chars = s2.ToList();
-                res = "";
             }
 
-            return resInt;
-        }
-
-        private static string CommonChildReq2(string s1, List<char> chars, int v)
-        {
-            string res = "";
-            string tempres = "";
-            for (int i = v; i < s1.Length; i++)
-            {
-               
-                if (chars.Contains(s1[i]))
-                {
-                    chars.RemoveRange(0, chars.FindIndex((x => s1[i] == x)) + 1);
-                    tempres += s1[i];
-                    tempres += CommonChildReq2(s1, chars, i + 1);
-                }
-                if (res.Length < tempres.Length)
-                    res = tempres;
-
-            }
             return res;
         }
+
+       
     }
 }
