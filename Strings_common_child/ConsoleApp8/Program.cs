5d0030d091a714a2e3a4752793eae5a950c7e738
sabotage84@mail.ru <sabotage84@mail.ru>
2019-04-04 10:55:01 +0300

    Strings CommonChild req with save work but still slow

--- Strings_common_child/ConsoleApp8/Program.cs
+++ Strings_common_child/ConsoleApp8/Program.cs
@@ -10,12 +10,32 @@ namespace ConsoleApp8
     class CharInd
     {
         char ch;
-        int index;
+        int indexS1;
+        int indexS2;
 
-        public CharInd(char ch, int index)
+        public CharInd(char ch, int index1, int index2)
         {
             this.ch = ch;
-            this.index = index;
+            this.indexS1 = index1;
+            this.indexS2 = index2;
+        }
+        public override bool Equals(object obj)
+        {
+            CharInd temp = (CharInd)obj;
+
+            if (this.ch == temp.ch && indexS1 == temp.indexS1 && indexS2 == temp.indexS2)
+                return true;
+            else
+                return false;
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = -461525457;
+            hashCode = hashCode * -1521134295 + ch.GetHashCode();
+            hashCode = hashCode * -1521134295 + indexS1.GetHashCode();
+            hashCode = hashCode * -1521134295 + indexS2.GetHashCode();
+            return hashCode;
         }
     }
 
@@ -23,7 +43,10 @@ namespace ConsoleApp8
     {
         static void Main(string[] args)
         {
-           int t= commonChild("WEWOUCUIDGCGTRMEZEPXZFEJWISRSBBSYXAYDFEJJDLEBVHHKS", "FDAGCXGKCTKWNECHMRXZWMLRYUCOCZHJRRJBOAJOQJZZVUYXIC");
+            string s1 = "WEWOUCUIDGCGTRMEZEPXZFEJWISRSBBSYXAYDFEJJDLEBVHHKS";
+            string s2 = "FDAGCXGKCTKWNECHMRXZWMLRYUCOCZHJRRJBOAJOQJZZVUYXIC";
+            
+            int t= commonChild(s1, s2);
             //commonChild("SHINCHAN", "NOHARAAA");
             Console.WriteLine(t);
             Console.ReadKey();
@@ -32,46 +55,55 @@ namespace ConsoleApp8
         {
             int res = 0;
             string tempS1 = "";
+            Dictionary<CharInd, int> dic = new Dictionary<CharInd, int>();
             string tempS2 = "";
-            for (int i = 0; i < s1.Length; i++)
-            {
-                if (s2.Contains(s1[i]))
-                    tempS1 += s1[i];
-            }
+            //for (int i = 0; i < s1.Length; i++)
+            //{
+            //    if (s2.Contains(s1[i]))
+            //        tempS1 += s1[i];
+            //}
 
-            for (int i = 0; i < s2.Length; i++)
-            {
-                if (s1.Contains(s2[i]))
-                    tempS2 += s2[i];
-            }
-
-            res = CommonChildReq3(tempS1, tempS2);
+            //for (int i = 0; i < s2.Length; i++)
+            //{
+            //    if (s1.Contains(s2[i]))
+            //        tempS2 += s2[i];
+            //}
+            //s1 = tempS1;
+            //s2 = tempS2;
+            res = CommonChildReq3(s1, s2,0,0,dic);
 
             return res;
         }
 
-        private static int CommonChildReq3(string s1, string s2)
+        private static int CommonChildReq3(string s1, string s2, int start1,int start2, Dictionary<CharInd, int> dic)
         {
             int res = 0;
             int tempRes = 0;
             List<char> alredyDone = new List<char>();
             
-            for (int i =0; (i < s1.Length && res <= (s1.Length - i)); i++)
+            for (int i =start1; (i < s1.Length && res <= (s1.Length - i)); i++)
             {
                 if (!alredyDone.Contains(s1[i]))
                 {
                     alredyDone.Add(s1[i]);
                     List<char> alredyDone2 = new List<char>();
-                    for (int j = 0; (j < s2.Length && res <= (s2.Length - j)); j++)
+                    for (int j = start2; (j < s2.Length && res <= (s2.Length - j)); j++)
                     {
                         if (!alredyDone2.Contains(s2[j]))
                         {
                             alredyDone2.Add(s2[j]);
                             if (s1[i] == s2[j])
                             {
-                                tempRes++;
-                                if (s2.Length - j > res)
-                                    tempRes += CommonChildReq3(s1.Substring(i + 1), s2.Substring(j + 1));
+                                if (dic.ContainsKey(new CharInd(s1[i], i, j)))
+                                    tempRes = dic[new CharInd(s1[i], i, j)];
+                                else
+                                {
+                                    tempRes++;
+                                    if (s2.Length - j > res)
+                                       tempRes += CommonChildReq3(s1, s2,i+1,j+1, dic);
+
+                                    dic.Add(new CharInd(s1[i], i, j), tempRes);
+                                }
                             }
                             
                         }
