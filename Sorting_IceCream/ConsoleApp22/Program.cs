434d94473795f3c355173b23561252fd919e4066
sabotage84@mail.ru <sabotage84@mail.ru>
2019-04-08 16:52:47 +0300

    Sorting IceCream

---
+++ Sorting_IceCream/ConsoleApp22/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp22
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+
+        static void whatFlavors(int[] cost, int money)
+        {
+
+
+        }
+    }
+}
