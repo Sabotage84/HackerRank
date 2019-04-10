46eb063ec79a3fb73e17d5d86e8068fd4ced25c3
sabotage84@mail.ru <sabotage84@mail.ru>
2019-04-10 14:45:17 +0300

    Sorting IceCream DONE

--- Sorting_IceCream/ConsoleApp22/ConsoleApp22/Program.cs
+++ Sorting_IceCream/ConsoleApp22/ConsoleApp22/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApp22
     {
         public static void Main(string[] args)
         {
-            string[] arStr = File.ReadAllLines("all2.txt");
+            string[] arStr = File.ReadAllLines("all3.txt");
             int t = Convert.ToInt32(arStr[0]);
 
             int monInd = 1;
@@ -28,8 +28,8 @@ namespace ConsoleApp22
                         cost[i] = int.Parse(nm[i]);
                     }
                 }
-                    //Array.ConvertAll(arStr[tItr + 3].Split(' '), costTemp => Convert.ToInt32(costTemp));
-
+                //Array.ConvertAll(arStr[tItr + 3].Split(' '), costTemp => Convert.ToInt32(costTemp));
+                
                 whatFlavors(cost, money);
                 //int m = 412242546;
                 //string[] arStr = File.ReadAllLines("test2.txt");
@@ -48,33 +48,80 @@ namespace ConsoleApp22
 
         static void whatFlavors(int[] cost, int money)
         {
-            int id1=-1, id2=-1;
-            for (int i = 0; i < cost.Length; i++)
+            int id1 = -1, id2 = -1;
+            int[] tempArr= new int[cost.Length];
+            cost.CopyTo(tempArr,0);
+            Array.Sort(tempArr);
+            for (int i = 0; i < tempArr.Length; i++)
             {
-                int temp = money - cost[i];
-                int tempMin = int.MaxValue;
-                for (int j = i+1; j < cost.Length; j++)
+                int temp = money - tempArr[i];
+                int left = i + 1;
+                int right = tempArr.Length - 1;
+
+                while (left <= right)
                 {
-                    if(cost[j]==temp)
+                    int mid = left + ((right - left) / 2);
+                    if (tempArr[mid] == temp)
                     {
-                        id1 = i;
-                        id2 = j;
-                        Console.WriteLine("{0} {1} ", id1 + 1, id2 + 1);
+
+                        if (tempArr[i] == tempArr[mid])
+                        {
+                            id1 = Array.FindIndex(cost, x => x == tempArr[i]);
+                            id2 = Array.FindIndex(cost, id1+1 , y => y == tempArr[mid]);
+                        }
+                        else
+                        {
+                            id1 = Array.FindIndex(cost, x => x == tempArr[i]);
+                            id2 = Array.FindIndex(cost, y => y == tempArr[mid]);
+                        }
+                        if (id1 > id2)
+                        {
+                            int tt = id1;
+                            id1 = id2;
+                            id2 = tt;
+                        }
+                           
+
+                        Console.WriteLine("{0} {1}", id1 + 1, id2 + 1);
                         return;
                     }
+                    else if (temp < tempArr[mid])
+                        right = mid - 1;
                     else
-                    {
-                        if(tempMin>(money-cost[i]-cost[j]) && (money - cost[i] - cost[j])>0)
-                        {
-                            tempMin = money - cost[i] - cost[j];
-                            id1 = i;
-                            id2 = j;
-                        }
-                    }
-
+                        left = mid + 1;
                 }
             }
-            Console.WriteLine("{0} {1} ", id1+1,id2+1);
         }
+
+        //static void whatFlavors(int[] cost, int money)
+        //{
+        //    int id1 = -1, id2 = -1;
+        //    for (int i = 0; i < cost.Length; i++)
+        //    {
+        //        int temp = money - cost[i];
+        //        int tempMin = int.MaxValue;
+        //        for (int j = i + 1; j < cost.Length; j++)
+        //        {
+        //            if (cost[j] == temp)
+        //            {
+        //                id1 = i;
+        //                id2 = j;
+        //                Console.WriteLine("Total overlap {0} {1}", id1 + 1, id2 + 1);
+        //                return;
+        //            }
+        //            else
+        //            {
+        //                if (tempMin > (money - cost[i] - cost[j]) && (money - cost[i] - cost[j]) > 0)
+        //                {
+        //                    tempMin = money - cost[i] - cost[j];
+        //                    id1 = i;
+        //                    id2 = j;
+        //                }
+        //            }
+
+        //        }
+        //    }
+        //    Console.WriteLine("{0} {1} ", id1 + 1, id2 + 1);
+        //}
     }
 }
