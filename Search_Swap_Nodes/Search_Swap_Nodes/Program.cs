d0b26fcff22c51cc6f48b611b402247b50990523
sabotage84@mail.ru <sabotage84@mail.ru>
2019-04-11 16:48:43 +0300

    Search SwapNodes ADD Treenode class

--- Search_Swap_Nodes/Search_Swap_Nodes/Program.cs
+++ Search_Swap_Nodes/Search_Swap_Nodes/Program.cs
@@ -13,31 +13,53 @@ namespace Search_Swap_Nodes
         }
     }
 
+    class TreeOfNodes
+    {
+        int[] numbers;
+
+        public TreeOfNodes(int[][] matrix)
+        {
+
+        }
+    }
+
+
     public class Node
     {
         int root;
+        Node parent;
         Node left;
         Node right;
 
         public Node()
         {
             root = 1;
+            parent = null;
             left = null;
             right = null;
         }
 
-        public Node(int root)
+        public Node(int root, Node p)
         {
+            this.parent = p;
             this.root = root;
             left = null;
             right = null;
         }
 
-        public Node(int root, Node left, Node right)
+        public Node(int root,Node p, Node left, Node right)
         {
+            this.parent = p;
             this.root = root;
             this.left = left;
             this.right = right;
+        }
+
+        public void Swap()
+        {
+            Node temp = left;
+            left = right;
+            right = temp;
         }
     }
 }
