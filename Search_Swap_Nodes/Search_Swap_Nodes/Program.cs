ce0933da1d7aee96eecf18822300da1328d70db2
sabotage84@mail.ru <sabotage84@mail.ru>
2019-04-16 16:50:26 +0300

    Search SwapNodes ADD Treenode simple show OK

--- Search_Swap_Nodes/Search_Swap_Nodes/Program.cs
+++ Search_Swap_Nodes/Search_Swap_Nodes/Program.cs
@@ -10,36 +10,44 @@ namespace Search_Swap_Nodes
     {
         static void Main(string[] args)
         {
+
+            
+            int[,] m =  { {2, 3 },
+                        { -1, -1 },
+                         {-1 ,-1 }
+                        };
+            TreeOfNodes tree = new TreeOfNodes(m);
+            tree.ShowTree();
         }
     }
 
     class TreeOfNodes
     {
         int[] numbers;
-       
+        Node rootNode = new Node(1,null);
         
-        public TreeOfNodes(int[][] matrix)
+        public TreeOfNodes(int[,] matrix)
         {
             int temp = 1;
-            Node rootNode = new Node();
+            
             List<Node> tempNodes = new List<Node>();
             tempNodes.Add(rootNode);
 
-            for (int i = 0; i < matrix[0].Length; i+=temp)
+            for (int i = 0; i < matrix.GetUpperBound(0) + 1; i+=temp)
             {
 
                 List<Node> tempNodes2 = tempNodes;
                 for (int j = i, k=0; j < i+temp; j++,k++)
                 {
                     
-                    int left = matrix[j][0];
+                    int left = matrix[j,0];
                     if (left != -1)
                     {
                         tempNodes2[k].left = new Node(left, tempNodes2[k]);
                         tempNodes.Add(tempNodes2[k].left);
                         temp++;
                     }
-                    int rght = matrix[j][1];
+                    int rght = matrix[j,1];
                     if (rght != -1)
                     {
                         tempNodes2[k].right = new Node(rght, tempNodes2[k]);
@@ -50,12 +58,63 @@ namespace Search_Swap_Nodes
                 tempNodes2 = null;
             }
         }
+
+        public void ShowTree()
+        {
+            List<int> lst = new List<int>();
+            if (rootNode == null)
+                Console.WriteLine("Tree is empty!");
+            else
+            {
+                
+                lst.Add(rootNode.root);
+                if (rootNode.left!=null)
+                    ShowLeft(lst, rootNode, rootNode.left);
+                if (rootNode.right != null)
+                    ShowRight(lst, rootNode, rootNode.right);
+
+            }
+            Console.WriteLine();
+            foreach (var item in lst)
+            {
+                Console.Write(item);
+            }
+            Console.ReadKey();
+        }
+
+        private void ShowRight(List<int> lst,Node r, Node right)
+        {
+            lst.Insert(lst.IndexOf(r.root) + 1, right.root);
+            Console.WriteLine(right.root);
+            if (right.left != null)
+            {
+                ShowLeft(lst, right, right.left);
+            }
+            if (right.right != null)
+            {
+                ShowRight(lst, right, right.right);
+            }
+        }
+
+        private void ShowLeft(List<int> lst, Node r, Node n)
+        {
+            lst.Insert(lst.IndexOf(r.root), n.root);
+            Console.WriteLine(n.root);
+            if (n.left!=null)
+            {
+                ShowLeft(lst, n, n.left);
+            }
+            if (n.right != null)
+            {
+                ShowRight(lst,n, n.right);
+            }
+        }
     }
 
 
     public class Node
     {
-        int root;
+        public int root;
         Node parent;
         public Node left;
         public Node right;
