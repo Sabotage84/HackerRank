6a4af5e9fdf9bf5e9c28a456476b19cea35cf0af
sabotage84@mail.ru <sabotage84@mail.ru>
2019-05-24 16:20:50 +0300

    ADD min time req TIMEOUT

---
+++ min_time_req/ConsoleApp28/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //string[] arStr = File.ReadAllLines("a.txt");
+            //int[] a = Array.ConvertAll(arStr[0].Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+            long item = 10;
+            long[] a = { 1, 3, 4 };
+
+
+            Console.WriteLine(triplets(a, item));
+            Console.ReadKey();
+        }
+
+        static long triplets(long[] machines, long goal)
+        {
+            long days = 0;
+            Dictionary<long, long> dic = new Dictionary<long, long>();
+            Array.Sort(machines);
+
+
+            for (int i = 0; i < machines.Length; i++)
+            {
+                if (dic.ContainsKey(machines[i]))
+                    dic[machines[i]]++;
+                else
+                {
+                    dic.Add(machines[i], 1);
+                }
+
+            }
+
+            while (!(goal<= 0))
+            {
+                days++;
+                foreach (var item in dic)
+                
+                {
+                    if (item.Key > days)
+                        break;
+                    else
+                    {
+                        if (days % item.Key == 0)
+                        {
+                            goal-=item.Value;
+                            if (goal <= 0)
+                                break;
+                        }
+                    }
+
+                }
+
+            }
+
+            return days;
+
+        }
+
+        static int NearestSearching(int[] arr, int num)
+        {
+            int res = -1;
+            int left = 0;
+            int right = arr.Length - 1;
+            //bool founded = false;
+            int med = right / 2;
+
+            while (left <= right)
+            {
+                if (arr[med] <= num && arr[med + 1] > num)
+                    return med;
+                else if (arr[med] > num)
+                {
+                    right = med;
+                    med = left + (med - left) / 2;
+
+                }
+                else
+                {
+                    left = med;
+                    med = med + (right - med) / 2;
+
+                }
+                //Console.WriteLine(med);
+            }
+            return res;
+        }
+    }
+}
