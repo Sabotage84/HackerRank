8dbeae3b970cf0c0ffca72e3204c896115973095
Sabotage84 <sabotage84@mail.ru>
2019-07-24 16:28:07 +0300

    ADD Maximum Subarray Sum v0_1

---
+++ Maximum Subarray Sum/ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long[] arr = { 3, 3, 9, 9, 5 };
+            long M = 7;
+            Console.WriteLine(maximumSum(arr, M));
+            Console.ReadKey();
+
+        }
+        static long maximumSum(long[] a, long m)
+        {
+            long res = 0;
+            long[] tempArr = new long[a.Length];
+            for (int i = 0; i < a.Length; i++)
+            {
+                tempArr[i] = a[i] % m;
+            }
+
+            long f = 0, s = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                f += a[i];
+                s += tempArr[i];
+
+            }
+            Console.WriteLine(f % m);
+            Console.WriteLine(s % m);
+            return res;
+        }
+    }
+}
