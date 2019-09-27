18d207c2873906792951c97ae1f19100f45d651c
Sabotage84 <sabotage84@mail.ru>
2019-09-27 15:07:18 +0300

    ADD WA stairs

---
+++ Staircase/Staircase/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Staircase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            staircase(10);
+            Console.ReadKey();
+        }
+
+        static void staircase(int n)
+        {
+            for (int i = 1; i <=n; i++)
+            {
+                Console.Write(new string(' ', n-i));
+                Console.WriteLine(new string('#', i));
+            }
+
+        }
+    }
+}
