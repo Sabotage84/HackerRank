57c99fc5cb9290fa335f187feff855942b1bbf8a
Sabotage84 <sabotage84@mail.ru>
2019-10-01 14:38:34 +0300

    ADD BirthdayCakeCandles

---
+++ BirthdayCakeCandles/BirthdayCakeCandles/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BirthdayCakeCandles
+{
+    class Program
+    { 
+
+        static void Main(string[] args)
+        {
+            int[] arr = { 1,2,3,4,3,2,1,5,4,4,5};
+            Console.WriteLine(birthdayCakeCandles(arr));
+            Console.ReadKey();
+        }
+        static int birthdayCakeCandles(int[] ar)
+        {
+            int res = 1;
+            int max = ar[0];
+            for (int i = 1; i < ar.Length; i++)
+            {
+
+                if (max<ar[i])
+                {
+                    max = ar[i];
+                    res = 1;
+                }
+                else if (max == ar[i])
+                    res++;
+            }
+            return res;
+        }
+    }
+}
