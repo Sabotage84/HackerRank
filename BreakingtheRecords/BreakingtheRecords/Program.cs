9b785638826fd17e4d1db8b92e3a2e2a70186350
Sabotage84 <sabotage84@mail.ru>
2019-10-10 22:55:34 +0300

    Add records

---
+++ BreakingtheRecords/BreakingtheRecords/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BreakingtheRecords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 10, 5, 20, 20, 4, 5, 2, 25, 1 };
+            int[] r= breakingRecords(arr);
+            for (int i = 0; i < r.Length; i++)
+            {
+                Console.WriteLine(r[i]);
+            }
+            Console.ReadKey();
+        }
+
+        static int[] breakingRecords(int[] scores)
+        {
+            int[] res = { 0, 0 };
+            int min = scores[0];
+            int max = scores[0];
+            for (int i = 0; i < scores.Length; i++)
+            {
+                if (min>scores[i])
+                {
+                    min = scores[i];
+                    res[1]++;
+                }
+                if (max<scores[i])
+                {
+                    max = scores[i];
+                    res[0]++;
+                }
+            }
+
+
+            return res;
+
+        }
+    }
+}
