9b33beae99f4700708127ec2b45888ad17a3b605
Sabotage84 <sabotage84@mail.ru>
2019-10-14 17:22:05 +0300

    ADD BonAppetit

---
+++ BonAppetit/BonAppetit/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BonAppetit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        static void bonAppetit(List<int> bill, int k, int b)
+        {
+
+            int sum = 0;
+            for (int i = 0; i < bill.Count; i++)
+            {
+                if (i == k)
+                    continue;
+                sum += bill[i];
+            }
+            sum = sum / 2;
+            if (b - sum == 0)
+                Console.WriteLine("Bon Appetit");
+            else
+                Console.WriteLine(b - sum);
+        }
+    }
+}
