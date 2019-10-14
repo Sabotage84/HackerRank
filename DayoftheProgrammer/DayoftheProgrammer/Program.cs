3e00ae0b4c58f2bc9d915b52299d908481014dc7
Sabotage84 <sabotage84@mail.ru>
2019-10-14 16:40:34 +0300

    ADD ProgDay

---
+++ DayoftheProgrammer/DayoftheProgrammer/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DayoftheProgrammer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int y = 2017;
+            Console.WriteLine(dayOfProgrammer(y));
+            Console.ReadKey();
+        }
+
+        static string dayOfProgrammer(int year)
+        {
+
+            if(year == 1918)
+                return "27.09.1918";
+            if (year < 1918)
+            {
+                if (year % 4 == 0)
+                    return "12.09." + year;
+                else
+                    return "13.09." + year;
+            }
+            else
+            {
+                if(year % 400 == 0)
+                    return "12.09." + year;
+                else if (year % 4 == 0 && year%100!=0)
+                    return "12.09." + year;
+                else
+                    return "13.09." + year;
+            }
+           
+        }
+    }
+}
