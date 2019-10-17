42f0c787c971b817ab773edcf2c2ca675e7691e4
Sabotage84 <sabotage84@mail.ru>
2019-10-17 23:19:52 +0300

    Add cat and mouse

---
+++ CatsandaMouse/CatsandaMouse/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatsandaMouse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static string catAndMouse(int x, int y, int z)
+        {
+            if (Math.Abs(z - x) < Math.Abs(z - y))
+                return "Cat A";
+            else if (Math.Abs(z - x) == Math.Abs(z - y))
+                return "Mouse C";
+            else
+                return "Cat B";
+
+        }
+    }
+}
