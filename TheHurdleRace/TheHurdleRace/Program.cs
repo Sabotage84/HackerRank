0ba6cd7243d1e032c125c9ff9375b4cff44fb719
Sabotage84 <sabotage84@mail.ru>
2019-10-22 16:50:33 +0300

    ADD The Hurdle Race

---
+++ TheHurdleRace/TheHurdleRace/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheHurdleRace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ar = { 1, 6, 3, 5, 2 };
+            int j = 4;
+            Console.WriteLine(hurdleRace(j, ar));
+            Console.ReadKey();
+        }
+        static int hurdleRace(int k, int[] height)
+        {
+            int res = height[0];
+            for (int i = 0;   i <height.Length;  i++)
+            {
+                if (res < height[i])
+                    res = height[i];
+            }
+
+            if (res < k)
+                return 0;
+            else
+                return res-k;
+        }
+
+
+    }
+}
