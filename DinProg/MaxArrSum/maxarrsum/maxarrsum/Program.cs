fb6e5b2b0c3e7451d58fb77d74f6b5e1915e918a
Sabotage84 <sabotage84@mail.ru>
2019-11-21 00:18:42 +0300

    TRY matrix work only positive num

--- DinProg/MaxArrSum/maxarrsum/maxarrsum/Program.cs
+++ DinProg/MaxArrSum/maxarrsum/maxarrsum/Program.cs
@@ -17,27 +17,42 @@ namespace maxarrsum
 
         static int maxSubsetSum(int[] arr)
         {
-            int res = 0;
-            int oldMax;
-            int newMax;
-            int oldIndex;
-            int newIndex;
-
-
-            if (arr[0]>=arr[0]+arr[2])
-            {
-                oldMax = arr[0];
-                oldIndex = 0;
-            }
-            else
-            {
-                oldMax = arr[0] + arr[2];
-                oldIndex = 2;
-            }
-
-            if()
             
-            return res;
+            int[,] matrix = new int[2, arr.Length];
+
+            matrix[0, 0] = arr[0];
+            matrix[0, 1] = Math.Max(matrix[0,0],arr[1]);
+            matrix[0, 2] = Math.Max(matrix[0, 1], arr[2]);
+
+            matrix[0, 3] = Math.Max(matrix[0, 2], arr[3]);
+            matrix[1, 2] = arr[0] + arr[2];
+            matrix[1, 3] = matrix[0, 1] + arr[3];
+
+
+            for (int i = 4; i < arr.Length; i++)
+            {
+                matrix[0, i] = Math.Max(matrix[0, i - 1], arr[i]);
+                if (arr[i] <= 0)
+                {
+                    matrix[1, i] = Math.Max(matrix[1, i - 2], matrix[0, i - 2] + arr[i]);
+                }
+                else
+                    matrix[1, i] = Math.Max(matrix[1, i - 2] + arr[i], matrix[0, i - 2] + arr[i]);
+            }
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(matrix[0, i] + " ");
+
+            }
+            Console.WriteLine();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(matrix[1, i] + " ");
+
+            }
+            Console.WriteLine();
+            return Math.Max(matrix[1, arr.Length-1],matrix[1, arr.Length-2]);
         }
     }
 }
