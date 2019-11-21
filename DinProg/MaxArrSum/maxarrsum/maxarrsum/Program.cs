eb70919bd485300bde085c733745bbe8de3bed7f
Sabotage84 <sabotage84@mail.ru>
2019-11-21 17:08:56 +0300

    Work but 4 test fail

--- DinProg/MaxArrSum/maxarrsum/maxarrsum/Program.cs
+++ DinProg/MaxArrSum/maxarrsum/maxarrsum/Program.cs
@@ -24,43 +24,22 @@ namespace maxarrsum
 
         static int maxSubsetSum(int[] arr)
         {
-            
-            int[,] matrix = new int[2, arr.Length];
-
-            matrix[0, 0] = arr[0];
-            matrix[0, 1] = Math.Max(matrix[0,0],arr[1]);
-            matrix[0, 2] = Math.Max(matrix[0, 1], arr[2]);
-
-            matrix[0, 3] = Math.Max(matrix[0, 2], arr[3]);
-            matrix[1, 2] = arr[0] + arr[2];
-            matrix[1, 3] = matrix[0, 1] + arr[3];
-
-
-            for (int i = 4; i < arr.Length; i++)
+            int[] C = new int[arr.Length];
+            C[0] = arr[0];
+            C[1] = arr[1];
+            if (arr[2] > 0)
+                C[2] = Math.Max(arr[2], arr[2] + C[0]);
+            else
+                C[2] = C[0];
+            for (int i = 3; i < arr.Length; i++)
             {
-                matrix[0, i] = Math.Max(matrix[0, i - 1], arr[i]);
-                if (arr[i] <= 0)
-                {
-                    matrix[1, i] = Math.Max(matrix[1, i - 2], matrix[1, i - 1]);
-                    matrix[1, i - 1] = matrix[1, i];
-                }
+                if (arr[i] > 0)
+                    C[i] = Math.Max(C[i - 2], C[i - 3]) + arr[i];
                 else
-                    matrix[1, i] = Math.Max(matrix[1, i - 2] + arr[i], matrix[0, i - 2] + arr[i]);
+                    C[i] = Math.Max(C[i - 2], C[i - 3]);
             }
 
-            for (int i = 0; i < arr.Length; i++)
-            {
-                Console.Write(matrix[0, i] + " ");
-
-            }
-            Console.WriteLine();
-            for (int i = 0; i < arr.Length; i++)
-            {
-                Console.Write(matrix[1, i] + " ");
-
-            }
-            Console.WriteLine();
-            return Math.Max(matrix[1, arr.Length-1],matrix[1, arr.Length-2]);
+            return Math.Max(C[C.Length - 1], C[C.Length - 2]);
         }
     }
 }
