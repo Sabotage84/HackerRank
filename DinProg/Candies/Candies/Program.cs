8dabf3c3346e2c15e5e091efd3a06ae48b0e45e6
Sabotage84 <sabotage84@mail.ru>
2019-12-03 17:07:44 +0300

    ADD DIN PROG Candies

---
+++ DinProg/Candies/Candies/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Candies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static long candies(int n, int[] arr)
+        {
+            long res = 0;
+            long temp = 0;
+
+            if (arr[0] >= arr[1])
+            {
+                temp = 1;
+            }
+            else
+                temp = 2;
+
+            res += 3;
+
+            for (int i = 2; i < arr.Length; i++)
+            {
+                if (arr[i]<)
+            }
+
+            return res;
+        }
+    }
+}
