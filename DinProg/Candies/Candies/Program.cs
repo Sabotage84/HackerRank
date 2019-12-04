a126c8f6f11610f99f698fa9befc8173ba0d5943
Sabotage84 <sabotage84@mail.ru>
2019-12-04 17:23:06 +0300

    ADD DIN PROG Candies done

--- DinProg/Candies/Candies/Program.cs
+++ DinProg/Candies/Candies/Program.cs
@@ -35,137 +35,35 @@ namespace Candies
 
         static long candies(int n, int[] arr)
         {
-            return Math.Max(foo(arr), foo(arr.Reverse().ToArray()));
-        }
-
-        static long foo(int[] arr)
-        {
             long res = 0;
-            long[,] candiesArr = new long[2, arr.Length];
+            long[] t = new long[arr.Length];
+            t[0] = 1;
 
-            if (arr[0] < arr[1])
-            {
-                candiesArr[0, 1] = 1;
-                candiesArr[1, 0] = 1;
-                candiesArr[1, 1] = 2;
-
-            }
-            else if (arr[0] > arr[1])
-            {
-                candiesArr[0, 1] = 0;
-                candiesArr[1, 0] = 1;
-                candiesArr[1, 1] = 2;
-            }
-            else
-            {
-                candiesArr[0, 1] = 3;
-                candiesArr[1, 0] = 1;
-                candiesArr[1, 1] = 1;
-            }
-
-
-
-            for (int i = 2; i < arr.Length; i++)
+            for (int i = 1; i < arr.Length; i++)
             {
                 if (arr[i] > arr[i - 1])
-                {
-
-                    if (candiesArr[0, i - 1] == 1)
-                    {
-                        candiesArr[0, i] = 1;
-                        candiesArr[1, i] = candiesArr[1, i - 1] + 1;
-                    }
-                    else if (candiesArr[0, i - 1] == 0)
-                    {
-                        candiesArr[1, i] = 1;
-
-                        if (i + 1 <= arr.Length - 1)
-                        {
-                        
-                            if (arr[i] < arr[i + 1])
-                            {    
-                                candiesArr[1, i + 1] = 2;
-                                candiesArr[0, i + 1] = 1;
-                            }
-                            else if(arr[i] > arr[i + 1])
-                            {
-                                candiesArr[1, i + 1] = 2;
-                                candiesArr[0, i + 1] = 0;
-                            }
-                            else
-                            {
-                                candiesArr[1, i + 1] = 1;
-                                candiesArr[0, i + 1] = 3;
-                            }
-                        }
-                        i++;
-                    }
-                    else
-                    {
-                        candiesArr[0, i] = 1;
-                        candiesArr[1, i] = candiesArr[1, i - 1] + 1;
-                    }
-                }
-                else if (arr[i] < arr[i - 1])
-                {
-
-                    if (candiesArr[0, i - 1] == 0)
-                    {
-                        candiesArr[0, i] = 0;
-                        candiesArr[1, i] = candiesArr[1, i - 1] + 1;
-                    }
-                    else if (candiesArr[0, i - 1] == 1)
-                    {
-                        candiesArr[1, i] = 1;
-
-                        if (i + 1 <= arr.Length - 1)
-                        {
-
-                            if (arr[i] < arr[i + 1])
-                            {
-                                candiesArr[1, i + 1] = 2;
-                                candiesArr[0, i + 1] = 1;
-                            }
-                            else if (arr[i] > arr[i + 1])
-                            {
-                                candiesArr[1, i + 1] = 2;
-                                candiesArr[0, i + 1] = 0;
-                            }
-                            else
-                            {
-                                candiesArr[1, i + 1] = 1;
-                                candiesArr[0, i + 1] = 3;
-                            }
-                        }
-                       
-                        i++;
-
-                    }
-                    else
-                    {
-                        candiesArr[0, i] = 0;
-                        candiesArr[1, i] = candiesArr[1, i - 1] + 1;
-                    }
-                }
+                    t[i] = t[i - 1] + 1;
                 else
-                {
-                    candiesArr[0, i] = 3;
-                    candiesArr[1, i] = 1;
-                }
+                    t[i] = 1;
             }
-            
-            for (int i = 0; i < arr.Length; i++)
+
+            for (int i = arr.Length - 2; i > -1; i--)
             {
-                //Console.WriteLine(candiesArr[1, i]);
-                res += candiesArr[1, i];
+                if (arr[i] > arr[i + 1] && t[i]<=t[i+1])
+                    t[i] = t[i + 1] + 1;
+               
+
+
             }
 
-            Console.WriteLine();
-            Console.WriteLine();
-            Console.WriteLine();
+            for (int i = 0; i < t.Length; i++)
+            {
+                res += t[i];
+            }
 
-            Console.WriteLine(res);
             return res;
         }
+
+        
     }
 }
