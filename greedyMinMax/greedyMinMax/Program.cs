f52a21a76ec912624c30fdf4271e3971a8cb9245
Sabotage84 <sabotage84@mail.ru>
2019-12-19 16:35:16 +0300

    ADD greedy MinMax

---
+++ greedyMinMax/greedyMinMax/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace greedyMinMax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static int maxMin(int k, int[] arr)
+        {
+           
+            Array.Sort(arr);
+            int res = arr[k-1]-arr[0];
+            for (int i = 0, j=k-1; j<arr.Length; i++, j++)
+            {
+                if (arr[j] - arr[i] < res)
+                    res = arr[j] - arr[i];
+
+            }
+            return res;
+        }
+
+    }
+}
