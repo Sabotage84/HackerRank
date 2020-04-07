87be90d9091c71efc32ba103ff1f8d6c62ec4f27
Sabotage84 <sabotage84@mail.ru>
2020-04-07 15:35:20 +0300

    Empty proj

---
+++ Find_the_nearest_clone/Find_the_nearest_clone/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Find_the_nearest_clone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static int findShortest(int graphNodes, int[] graphFrom, int[] graphTo, long[] ids, int val)
+        {
+            int res = -1;
+
+
+            return res;
+        }
+
+    }
+}
