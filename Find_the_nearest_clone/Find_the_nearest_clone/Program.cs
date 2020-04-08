b9d597ae3dd418d70238416692cfa9ee456f6ce9
Sabotage84 <sabotage84@mail.ru>
2020-04-08 23:32:31 +0300

    ADD node class

--- Find_the_nearest_clone/Find_the_nearest_clone/Program.cs
+++ Find_the_nearest_clone/Find_the_nearest_clone/Program.cs
@@ -12,8 +12,14 @@ namespace Find_the_nearest_clone
         {
             string s = "1 2 1 1";
             long[] ids = Array.ConvertAll(s.Split(' '), idsTemp => Convert.ToInt64(idsTemp));
+            //long[] ids = Array.ConvertAll(Console.ReadLine().Trim(' ').Split(' '), idsTemp => Convert.ToInt64(idsTemp))
             Console.WriteLine(ids.Length);
             Console.ReadLine();
+        }
+        class Node
+        {
+            public long color;
+            public int[] neighbors;
         }
 
         static int findShortest(int graphNodes, int[] graphFrom, int[] graphTo, long[] ids, int val)
@@ -21,13 +27,27 @@ namespace Find_the_nearest_clone
             int res = 1;
             long tempVal = val;
             int tempRes = 0;
+            Node[] nodes = new Node[graphNodes];
+            Queue<int> nodesForCheck = new Queue<int>();
+
+            nodesForCheck.Enqueue(3); // очередь 3
+            nodesForCheck.Enqueue(5); // очередь 3, 5
+            nodesForCheck.Enqueue(8); // очередь 3, 5, 8
+
+            // получаем первый элемент очереди
+            int queueElement = nodesForCheck.Dequeue(); //теперь очередь 5, 8
+
             for (int i = 0; i < ids.Length; i++)
             {
+                nodes[i].color = ids[i];
                 if (tempVal == ids[i])
                     tempRes++;
             }
             if (tempRes >= 2)
+            {
+
                 return res;
+            }
             else
                 return -1;
         }
