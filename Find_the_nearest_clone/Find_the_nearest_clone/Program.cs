54ca207797bbca2249902822bda27de1355d240e
Sabotage84 <sabotage84@mail.ru>
2020-04-08 23:08:54 +0300

    FIX input err

--- Find_the_nearest_clone/Find_the_nearest_clone/Program.cs
+++ Find_the_nearest_clone/Find_the_nearest_clone/Program.cs
@@ -10,17 +10,26 @@ namespace Find_the_nearest_clone
     {
         static void Main(string[] args)
         {
+            string s = "1 2 1 1";
+            long[] ids = Array.ConvertAll(s.Split(' '), idsTemp => Convert.ToInt64(idsTemp));
+            Console.WriteLine(ids.Length);
+            Console.ReadLine();
         }
 
         static int findShortest(int graphNodes, int[] graphFrom, int[] graphTo, long[] ids, int val)
         {
-            int res = -1;
+            int res = 1;
+            long tempVal = val;
+            int tempRes = 0;
             for (int i = 0; i < ids.Length; i++)
             {
-
+                if (tempVal == ids[i])
+                    tempRes++;
             }
-
-            return res;
+            if (tempRes >= 2)
+                return res;
+            else
+                return -1;
         }
 
     }
