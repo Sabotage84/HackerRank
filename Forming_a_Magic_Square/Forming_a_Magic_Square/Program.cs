31c103d7a5c9a24ab1a5bfe3da21a71613b255ad
Sabotage84 <sabotage84@mail.ru>
2020-04-22 06:54:57 +0300

    ADD check martix meth and tests

--- Forming_a_Magic_Square/Forming_a_Magic_Square/Program.cs
+++ Forming_a_Magic_Square/Forming_a_Magic_Square/Program.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Forming_a_Magic_Square
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
@@ -15,8 +15,35 @@ namespace Forming_a_Magic_Square
         static int formingMagicSquare(int[][] s)
         {
             int res = 0;
-
+            if (CheckMatrix(s))
+                return res;
+            else
+                res = CorrectMatrix();
             return res;
+        }
+
+        public static int CorrectMatrix()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool CheckMatrix(int[][] s)
+        {
+            
+            int firstLine = s[0][0] + s[0][1] + s[0][2];
+            int secondLine= s[1][0] + s[1][1] + s[1][2];
+            int thirdLine = s[2][0] + s[2][1] + s[2][2];
+
+            int firstCol= s[0][0] + s[1][0] + s[2][0];
+            int secondCol = s[0][1] + s[1][1] + s[2][1];
+            int thirdCol = s[0][2] + s[1][2] + s[2][2];
+
+            int firstDiag= s[0][0] + s[1][1] + s[2][2];
+            int secondDiag= s[2][0] + s[1][1] + s[0][2];
+            if (firstLine == secondLine && firstLine == thirdLine && firstLine == firstCol && firstLine == secondCol && firstLine == thirdCol && firstLine == firstDiag && firstLine == secondDiag)
+                return true;
+
+            return false;
         }
     }
 }
---
+++ Forming_a_Magic_Square/tests/UnitTest1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Forming_a_Magic_Square;
+
+namespace tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestCheckMatrixGoodMatrixs()
+        {
+            int[][] s1 =
+                {
+                    new int[] { 1, 1, 1},
+                    new int[] { 1, 1, 1},
+                    new int[] { 1, 1, 1}
+                };
+
+            Assert.AreEqual(true, Program.CheckMatrix(s1));
+
+            int[][] s2 =
+                {
+                    new int[] { 4, 9, 2},
+                    new int[] { 3, 5, 7},
+                    new int[] { 8, 1, 6}
+                };
+
+            Assert.AreEqual(true, Program.CheckMatrix(s2));
+
+
+        }
+        [TestMethod]
+        public void TestCheckMatrixbadMatrix()
+        {
+            int[][] s1 =
+                {
+                    new int[] { 1, 1, 1},
+                    new int[] { 1, 9, 1},
+                    new int[] { 1, 1, 1}
+                };
+
+            Assert.AreEqual(false, Program.CheckMatrix(s1));
+        }
+    }
+}
