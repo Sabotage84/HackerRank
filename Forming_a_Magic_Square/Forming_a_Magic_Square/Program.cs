132d6e62ff30626a663e778893670317253a0943
Sabotage84 <sabotage84@mail.ru>
2020-04-22 06:04:26 +0300

    ADD magic square

---
+++ Forming_a_Magic_Square/Forming_a_Magic_Square/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Forming_a_Magic_Square
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static int formingMagicSquare(int[][] s)
+        {
+            int res = 0;
+
+            return res;
+        }
+    }
+}
