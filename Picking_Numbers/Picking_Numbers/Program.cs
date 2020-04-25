c7b4cb21ac4dc7f7e0d8bd61d5d0186e240e2e46
Sabotage84 <sabotage84@mail.ru>
2020-04-25 15:51:03 +0300

    ADD picking Num proj

---
+++ Picking_Numbers/Picking_Numbers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Picking_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static int pickingNumbers(List<int> a)
+        {
+            int res = 0;
+            Dictionary<int, int> mass = new Dictionary<int, int>();
+           
+
+            return res;
+        }
+    }
+}
