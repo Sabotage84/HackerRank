0ba5e0e88a47371cf48d96818bae28d88e42499e
Sabotage84 <sabotage84@mail.ru>
2020-04-25 16:01:37 +0300

    picking Num DONE

--- Picking_Numbers/Picking_Numbers/Program.cs
+++ Picking_Numbers/Picking_Numbers/Program.cs
@@ -15,9 +15,16 @@ namespace Picking_Numbers
         public static int pickingNumbers(List<int> a)
         {
             int res = 0;
-            Dictionary<int, int> mass = new Dictionary<int, int>();
-           
-
+            int[] mass = new int[100];
+            foreach (var item in a)
+            {
+                mass[item]++;
+            }
+            for (int i = 0; i < mass.Length-1; i++)
+            {
+                if (mass[i] + mass[i + 1] > res)
+                    res = mass[i] + mass[i + 1];
+            }
             return res;
         }
     }
