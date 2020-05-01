add33052ca9d16c03218c3d7af8bb1841fb0636d
Sabotage84 <sabotage84@mail.ru>
2020-05-01 21:32:23 +0300

    ADD BeautifulDaysAtTheMovies

---
+++ BeautifulDaysAtTheMovies/BeautifulDaysAtTheMovies/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeautifulDaysAtTheMovies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static int beautifulDays(int i, int j, int k)
+        {
+
+
+        }
+    }
+}
