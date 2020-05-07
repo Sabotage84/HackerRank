9567c5902a640825c9d476c1c568317426a0164b
Sabotage84 <sabotage84@mail.ru>
2020-05-07 17:47:03 +0300

    viralAdvertising DONE

--- ViralAdvertising/ViralAdvertising/Program.cs
+++ ViralAdvertising/ViralAdvertising/Program.cs
@@ -14,8 +14,17 @@ namespace ViralAdvertising
 
         static int viralAdvertising(int n)
         {
+            int likes=2;
+            int res = 2;
+            int sends = 1;
+            for (int i = 2; i <= n; i++)
+            {
+                sends = likes * 3;
+                res += sends / 2;
+                likes = sends / 2;
+            }
 
-
+            return res;
         }
     }
 }
