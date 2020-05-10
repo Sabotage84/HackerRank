c0174aa11008db360ae70f16676c2a0cb4cf8871
Sabotage84 <sabotage84@mail.ru>
2020-05-10 15:59:29 +0300

    ADD Save the prisoner

---
+++ Save_the_Prisoner/Save_the_Prisoner/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Save_the_Prisoner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static int saveThePrisoner(int n, int m, int s)
+        {
+            int res = 0;
+
+            return res;
+        }
+    }
+}
