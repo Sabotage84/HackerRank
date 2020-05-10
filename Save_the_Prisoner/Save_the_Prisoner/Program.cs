a7fc40f92d4d36740daf6db18142b1888fe9fbab
Sabotage84 <sabotage84@mail.ru>
2020-05-10 16:26:40 +0300

    Save the prisoner DONE

--- Save_the_Prisoner/Save_the_Prisoner/Program.cs
+++ Save_the_Prisoner/Save_the_Prisoner/Program.cs
@@ -10,12 +10,17 @@ namespace Save_the_Prisoner
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(100 % 7);
+            Console.ReadKey();
         }
 
         static int saveThePrisoner(int n, int m, int s)
         {
             int res = 0;
-
+            int t = s - 1 + m % n;
+            res = t % n;
+            if (res == 0)
+                return n;
             return res;
         }
     }
