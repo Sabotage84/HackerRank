93938026d8c26611acd8e02143242bea698ef2d9
Sabotage84 <sabotage84@mail.ru>
2020-05-11 21:38:16 +0300

    ADD SequenceEquation

---
+++ SequenceEquation/SequenceEquation/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SequenceEquation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        static int[] permutationEquation(int[] p)
+        {
+
+
+        }
+    }
+}
