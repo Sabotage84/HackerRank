d4b1e12fcb6e265c0b85e5ad7a6fede996abbb9b
Sabotage84 <sabotage84@mail.ru>
2020-05-12 14:05:08 +0300

    SequenceEquation DONE

--- SequenceEquation/SequenceEquation/Program.cs
+++ SequenceEquation/SequenceEquation/Program.cs
@@ -10,10 +10,35 @@ namespace SequenceEquation
     {
         static void Main(string[] args)
         {
+            int[] t1= { 4, 3, 5, 1, 2 };
+            int[] r= permutationEquation(t1);
+            for (int i = 0; i < r.Length; i++)
+            {
+                Console.Write(r[i] + " ");
+            }
+            Console.ReadKey();
         }
         static int[] permutationEquation(int[] p)
         {
+            int[] res = new int[p.Length];
+            for (int i = 0; i < p.Length; i++)
+            {
+                int t = i + 1;
+                for (int j = 0; j < p.Length; j++)
+                {
+                    if (p[j]==t)
+                    {
+                        for (int k = 0; k < p.Length; k++)
+                        {
+                            if (p[k] == j+1)
+                                res[i] = k+1;
+                        }
+                    }
+                }
+            }
 
+
+            return res;
 
         }
     }
