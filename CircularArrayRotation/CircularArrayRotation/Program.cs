aa3ca66a073573a8deb57eda38d64586d1717efe
Sabotage84 <sabotage84@mail.ru>
2020-05-21 22:18:14 +0300

    holms task DONE

--- CircularArrayRotation/CircularArrayRotation/Program.cs
+++ CircularArrayRotation/CircularArrayRotation/Program.cs
@@ -10,11 +10,40 @@ namespace CircularArrayRotation
     {
         static void Main(string[] args)
         {
+            int[] A = {1,2,3,4,5,6,7};
+            for (int i = 0; i < A.Length; i++)
+            {
+                Console.Write(A[i] + " ");
+            }
+            Console.WriteLine();
+
+            A = circularArrayRotation(A, 3, A);
+            for (int i = 0; i < A.Length; i++)
+            {
+                Console.Write(A[i] + " ");
+            }
+            Console.WriteLine();
+            Console.ReadLine();
+
         }
 
         static int[] circularArrayRotation(int[] a, int k, int[] queries)
         {
-
+            int[] temp = new int[a.Length];
+            int[] res = new int[queries.Length];
+            for (int i = 0; i < a.Length; i++)
+            {
+                if ((i + k)%a.Length < a.Length)
+                    temp[(i + k)%a.Length] = a[i];
+                else
+                    temp[(i + k)%a.Length - a.Length] = a[i];
+            }
+            
+            for (int i = 0; i < queries.Length; i++)
+            {
+                res[i] = temp[queries[i]];
+            }
+            return res;
 
         }
     }
