31766922ea5c7923c6c8c232aae1d7d28522130d
Sabotage84 <sabotage84@mail.ru>
2020-05-21 21:30:14 +0300

    ADD holms task

---
+++ CircularArrayRotation/CircularArrayRotation/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircularArrayRotation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static int[] circularArrayRotation(int[] a, int k, int[] queries)
+        {
+
+
+        }
+    }
+}
