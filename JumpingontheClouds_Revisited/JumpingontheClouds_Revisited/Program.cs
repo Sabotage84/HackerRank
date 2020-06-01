6f7edfe9ea421ec84350b0f03bd65de17a3ff854
Sabotage84 <sabotage84@mail.ru>
2020-06-01 16:20:32 +0300

    clouds revisited done

--- JumpingontheClouds_Revisited/JumpingontheClouds_Revisited/Program.cs
+++ JumpingontheClouds_Revisited/JumpingontheClouds_Revisited/Program.cs
@@ -10,6 +10,10 @@ namespace JumpingontheClouds_Revisited
     {
         static void Main(string[] args)
         {
+            int[] c = { 0, 0, 1, 0, 0, 1, 1, 0 };
+            int k = 2;
+            Console.WriteLine(jumpingOnClouds(c, k));
+            Console.ReadKey();
         }
 
         static int jumpingOnClouds(int[] c, int k)
@@ -18,19 +22,29 @@ namespace JumpingontheClouds_Revisited
             int i= 0;
             while (true)
             {
-                if ((i+k)%(c.Length-1)==0)
+                if ((i+k)>c.Length-1)
                 {
-                    i = c.Length - 1;
+                    i = i + k - c.Length;
+                    if (i==0)
+                    {
+                        if (c[i] == 1)
+                            res -= 3;
+                        else
+                            res--;
+                        break;
+                    }
                 }
                 else
                 {
-                    i = (i + k) % (c.Length - 1);
+                    i = i + k;
                 }
+                 
+                
                 if (c[i] == 1)
                     res -= 3;
                 else
                     res--;
-                if (i==0)
+                
             }
 
             return res;
