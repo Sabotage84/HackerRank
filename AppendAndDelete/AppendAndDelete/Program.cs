f9fdef1aa66f3472bf18b0cb800f29b6272300a1
Sabotage84 <sabotage84@mail.ru>
2020-06-06 22:51:24 +0300

    APPEND and del pro

---
+++ AppendAndDelete/AppendAndDelete/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppendAndDelete
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static string appendAndDelete(string s, string t, int k)
+        {
+            int op = 0;
+            for (int i = 0; i < length; i++)
+            {
+
+            }
+
+        }
+    }
+}
