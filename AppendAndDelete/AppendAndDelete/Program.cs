99c14cb2ddbe4755304393ede8b9dd087ff36a10
Sabotage84 <sabotage84@mail.ru>
2020-06-07 18:46:26 +0300

    APPEND and del 2 case wrong

--- AppendAndDelete/AppendAndDelete/Program.cs
+++ AppendAndDelete/AppendAndDelete/Program.cs
@@ -10,15 +10,29 @@ namespace AppendAndDelete
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(appendAndDelete("ab", "abf", 0));
+            Console.ReadKey();
         }
 
         static string appendAndDelete(string s, string t, int k)
         {
             int op = 0;
-            for (int i = 0; i < length; i++)
+            for (int i = 0; i < s.Length; i++)
             {
+                if (s[i] != t[i])
+                {
+                    op += s.Length - i;
+                    op += t.Length - i;
+                    break;
+                }
 
             }
+            if (op == 0)
+                op += t.Length - s.Length;
+            if (op > k)
+                return "NO";
+            else
+                return "YES";
 
         }
     }
