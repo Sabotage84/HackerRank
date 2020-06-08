31dc07c48ac000ff664a325315936abd2814e348
Sabotage84 <sabotage84@mail.ru>
2020-06-08 22:33:33 +0300

    APPEND and del DONE

--- AppendAndDelete/AppendAndDelete/Program.cs
+++ AppendAndDelete/AppendAndDelete/Program.cs
@@ -10,29 +10,47 @@ namespace AppendAndDelete
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(appendAndDelete("ab", "abf", 0));
+            Console.WriteLine(appendAndDelete("hackerhappy", "hackerrank", 9));
             Console.ReadKey();
         }
 
         static string appendAndDelete(string s, string t, int k)
         {
-            int op = 0;
-            for (int i = 0; i < s.Length; i++)
-            {
-                if (s[i] != t[i])
-                {
-                    op += s.Length - i;
-                    op += t.Length - i;
-                    break;
-                }
-
-            }
-            if (op == 0)
-                op += t.Length - s.Length;
-            if (op > k)
-                return "NO";
+            int r = -1;
+            if (s == t)
+                r = s.Length;
             else
-                return "YES";
+            {
+                for (int i = 0; i < s.Length; i++)
+                {
+                    if (i < t.Length)
+                    {
+                        if (s[i] != t[i])
+                        {
+                            r = i;
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        r = t.Length;
+                        break;
+                    }
+                }
+            }
+            if (r == -1)
+                r = s.Length;
+            int d = t.Length - r+s.Length-r;
+            if (d > k)
+                return "No";
+            else if ((k - d) % 2 == 0)
+                return "Yes";
+            else if ((k - d) >= s.Length + t.Length)
+                return "Yes";
+            else if (k >= s.Length + t.Length)
+                return "Yes";
+            else
+                return "No";
 
         }
     }
