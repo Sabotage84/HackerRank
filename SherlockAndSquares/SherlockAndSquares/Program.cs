6b20a2e0f5c7ade0f5e9de7eb52d4a59e7ecb99a
Sabotage84 <sabotage84@mail.ru>
2020-06-09 21:39:42 +0300

    herlock and squares DONE

--- SherlockAndSquares/SherlockAndSquares/Program.cs
+++ SherlockAndSquares/SherlockAndSquares/Program.cs
@@ -14,7 +14,13 @@ namespace SherlockAndSquares
         static int squares(int a, int b)
         {
             int res = 0;
-
+            int b1 =(int) Math.Sqrt(a);
+            int b2 = (int)Math.Sqrt(b);
+            for (int i = b1; i <= b2; i++)
+            {
+                if (i * i >= a && i * i <= b)
+                    res++;
+            }
             return res;
 
         }
