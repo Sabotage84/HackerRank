f4ba7024dce5fb680bdb9d3004abeb2ae0eec3f7
Sabotage84 <sabotage84@mail.ru>
2020-06-09 21:32:45 +0300

    ADD herlock and squares proj

---
+++ SherlockAndSquares/SherlockAndSquares/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SherlockAndSquares
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        static int squares(int a, int b)
+        {
+            int res = 0;
+
+            return res;
+
+        }
+    }
+}
