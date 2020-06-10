d83d04a85229e2126a12e202bd652d84fd6c5ea8
Sabotage84 <sabotage84@mail.ru>
2020-06-10 21:41:36 +0300

    Library Fine DONE

--- LibraryFine/LibraryFine/Program.cs
+++ LibraryFine/LibraryFine/Program.cs
@@ -11,5 +11,21 @@ namespace LibraryFine
         static void Main(string[] args)
         {
         }
+
+        static int libraryFine(int d1, int m1, int y1, int d2, int m2, int y2)
+        {
+
+            if (y2 < y1)
+                return 10000;
+            else if (m2 < m1 && y2==y1)
+                return 500 * (m1 - m2);
+            else if (d2 < d1 && m1==m2 && y2==y1 )
+                return 15 * (d1 - d2);
+            else
+                return 0;
+
+            
+
+        }
     }
 }
