41f809b8eb52ecb65e900878581719da24a84ccf
Sabotage84 <sabotage84@mail.ru>
2020-06-17 22:01:06 +0300

    ADD cut the stick

---
+++ CutTheSticks/CutTheSticks/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CutTheSticks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static int[] cutTheSticks(int[] arr)
+        {
+            List<int> lres = new List<int>();
+
+            return lres.ToArray();
+        }
+    }
+}
