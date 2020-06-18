e8b2e89ccfda759712741b04b074dedb68a257b6
Sabotage84 <sabotage84@mail.ru>
2020-06-18 21:51:10 +0300

    equalizeArray DONE

--- EqualizeArray/EqualizeArray/Program.cs
+++ EqualizeArray/EqualizeArray/Program.cs
@@ -14,8 +14,25 @@ namespace EqualizeArray
 
         static int equalizeArray(int[] arr)
         {
+            int res = 0;
+            int max=1;
+            Dictionary<int, int> r = new Dictionary<int, int>();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (r.ContainsKey(arr[i]))
+                    r[arr[i]]++;
+                else
+                    r.Add(arr[i], 1);
+            }
+            
+            foreach (var item in r)
+            {
+                if (max < item.Value)
+                    max = item.Value;
+                res += item.Value;
+            }
 
-
+            return res - max;
         }
     }
 }
