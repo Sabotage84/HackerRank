612461449160f4dd4eb4382aedea42dcff1180bb
Sabotage84 <sabotage84@mail.ru>
2020-06-20 21:36:06 +0300

    ADD QueenAttack

---
+++ QueenAttack/QueenAttack/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueenAttack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static int queensAttack(int n, int k, int r_q, int c_q, int[][] obstacles)
+        {
+
+
+        }
+    }
+}
