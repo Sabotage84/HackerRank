e01ac9101e09355ba9a084d22c0e1bae131445c5
Sabotage84 <sabotage84@mail.ru>
2020-07-01 21:47:41 +0300

    add ACM ICPC Team

---
+++ ACM_ICPC/ACM_ICPC/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACM_ICPC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static int[] acmTeam(string[] topic)
+        {
+            int[] res = new int[2];
+
+
+            return res;
+        }
+    }
+}
