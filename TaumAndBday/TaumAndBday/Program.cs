a3cb5989d1e8715dcd31f95bbfd4c8a911ad49fd
Sabotage84 <sabotage84@mail.ru>
2020-07-02 21:59:57 +0300

    ADD Taum and B

---
+++ TaumAndBday/TaumAndBday/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaumAndBday
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        public static long taumBday(int b, int w, int bc, int wc, int z)
+        {
+            long res = 0;
+
+            return res;
+        }
+    }
+}
