1992d0467a54b0beb79c0033df9df8dc075efb43
Sabotage84 <sabotage84@mail.ru>
2020-07-06 22:27:33 +0300

    ADD service lane

---
+++ ServiceLane/ServiceLane/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceLane
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static int[] serviceLane(int n, int[][] cases, int[] width)
+        {
+            int[] res = new int[width.Length];
+
+
+            return res;
+        }
+    }
+}
