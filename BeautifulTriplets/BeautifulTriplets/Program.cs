1a2c2830dda860bc49e6ba31919a6ed6b5b04205
Sabotage84 <sabotage84@mail.ru>
2020-07-07 21:17:32 +0300

    ADD BeautifulTriplets

---
+++ BeautifulTriplets/BeautifulTriplets/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeautifulTriplets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static int beautifulTriplets(int d, int[] arr)
+        {
+            int res = 0;
+
+
+            return res;
+
+        }
+    }
+}
