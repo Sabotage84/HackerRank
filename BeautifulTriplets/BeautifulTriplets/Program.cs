3e7245574ddab7084ad3edab24a9043a2f8ff9db
Sabotage84 <sabotage84@mail.ru>
2020-07-07 21:27:27 +0300

    BeautifulTriplets DONE

--- BeautifulTriplets/BeautifulTriplets/Program.cs
+++ BeautifulTriplets/BeautifulTriplets/Program.cs
@@ -15,7 +15,34 @@ namespace BeautifulTriplets
         static int beautifulTriplets(int d, int[] arr)
         {
             int res = 0;
-
+            for (int i = 0; i < arr.Length-2; i++)
+            {
+                for (int j = i+1; j < arr.Length-1; j++)
+                {
+                    if (arr[j] == arr[i])
+                        continue;
+                    else if (arr[j] - arr[i] < d)
+                        continue;
+                    else if (arr[j] - arr[i] > d)
+                        break;
+                    else
+                    {
+                        for (int l = j+1; l < arr.Length; l++)
+                        {
+                            if (arr[l] - arr[j] < d)
+                                continue;
+                            else if (arr[l] - arr[j] > d)
+                                break;
+                            else
+                            {
+                                res++;
+                                break;
+                            }
+                        }
+                        break;
+                    }
+                }
+            }
 
             return res;
 
