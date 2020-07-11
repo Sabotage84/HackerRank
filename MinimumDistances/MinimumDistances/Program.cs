25a3fd3b8de8d7e0dcd0ef92dfbd8b3e90c949d9
Sabotage84 <sabotage84@mail.ru>
2020-07-11 22:21:27 +0300

    ADD MinimumDistances

---
+++ MinimumDistances/MinimumDistances/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinimumDistances
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static int minimumDistances(int[] a)
+        {
+            int res = 0;
+
+
+            return res;
+        }
+    }
+}
