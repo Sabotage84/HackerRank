ff1877cc8b68c12d35bec641ea482eb4b5920fd1
Sabotage84 <sabotage84@mail.ru>
2020-07-12 20:18:53 +0300

    MinimumDistances DONE

--- MinimumDistances/MinimumDistances/Program.cs
+++ MinimumDistances/MinimumDistances/Program.cs
@@ -14,9 +14,21 @@ namespace MinimumDistances
 
         static int minimumDistances(int[] a)
         {
-            int res = 0;
-
-
+            int res = a.Length;
+            for (int i = 0; i < a.Length-1; i++)
+            {
+                for (int j = i+1; j < a.Length; j++)
+                {
+                    if(a[i]==a[j])
+                    {
+                        if (j - i < res)
+                            res = j - i;
+                        break;
+                    }
+                }
+            }
+            if (res == a.Length)
+                res = -1;
             return res;
         }
     }
