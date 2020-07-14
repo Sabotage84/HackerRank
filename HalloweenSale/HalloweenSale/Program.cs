f8ee42e8a38575f6bf7fbb15ef027544e982dfcb
Sabotage84 <sabotage84@mail.ru>
2020-07-14 17:45:03 +0300

    Halloween Sale

---
+++ HalloweenSale/HalloweenSale/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HalloweenSale
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        static int howManyGames(int p, int d, int m, int s)
+        {
+            int res = 0;
+
+
+            return res;
+
+        }
+
+    }
+}
