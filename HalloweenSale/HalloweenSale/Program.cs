3d080ff7cedf840d2ece4199e9aa0412ee0d5f9f
Sabotage84 <sabotage84@mail.ru>
2020-07-15 17:55:35 +0300

    Halloween Sale half tests

--- HalloweenSale/HalloweenSale/Program.cs
+++ HalloweenSale/HalloweenSale/Program.cs
@@ -10,11 +10,21 @@ namespace HalloweenSale
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(howManyGames(20, 3, 6, 85));
+            Console.ReadLine();
         }
         static int howManyGames(int p, int d, int m, int s)
         {
             int res = 0;
-
+            while (s>=m)
+            {
+                s -= p;
+                res++;
+                if (p > m)
+                    p -= d;
+                else
+                    p = m;
+            }
 
             return res;
 
