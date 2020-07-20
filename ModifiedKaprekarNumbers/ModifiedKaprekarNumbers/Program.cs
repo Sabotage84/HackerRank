0b41417704342a62e3884b081eca81796d7ea0d1
Sabotage84 <sabotage84@mail.ru>
2020-07-20 22:31:51 +0300

    ModifiedKaprekarNumbers DONE

---
+++ ModifiedKaprekarNumbers/ModifiedKaprekarNumbers/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModifiedKaprekarNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            kaprekarNumbers(1, 99999);
+            Console.ReadKey();
+        }
+
+        static void kaprekarNumbers(int p, int q)
+        {
+            List<int> res = new List<int>();
+
+            for (int i = p; i <= q; i++)
+            {
+                if (IsKaprekar(i))
+                    res.Add(i);
+            }
+
+            if (res.Count == 0)
+                Console.WriteLine("INVALID RANGE");
+            else
+                foreach (var item in res)
+                {
+                    Console.Write(item + " ");
+                }
+        }
+
+        public static bool IsKaprekar(int a)
+        {
+            long sq = (long)a * (long)a;
+            string s = sq.ToString();
+            int r = 0;
+            int l = 0;
+
+            if (s.Length < 2)
+            {
+                r = int.Parse(s);
+            }
+            else
+            {
+                r = int.Parse(s.Substring(s.Length-a.ToString().Length));               
+                l = int.Parse(s.Substring(0, s.Length - (a.ToString().Length)));
+            }
+            if (l + r == a)
+                return true;
+            else 
+                return false;
+        }
+    }
+}
