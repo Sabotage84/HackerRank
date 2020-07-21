a86e2a1a31de1dd08e245bde2fe768d37e9cc824
Sabotage84 <sabotage84@mail.ru>
2020-07-21 23:14:15 +0300

    ADD LisasWorkbook

---
+++ LisasWorkbook/LisasWorkbook/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LisasWorkbook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static int workbook(int n, int k, int[] arr)
+        {
+            int res = 0;
+
+            return res;
+        }
+    }
+}
