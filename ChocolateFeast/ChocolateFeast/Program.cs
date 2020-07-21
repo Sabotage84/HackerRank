f4de7da0a675789043826a423e4080bd93ae9dc5
Sabotage84 <sabotage84@mail.ru>
2020-07-21 22:27:24 +0300

    Chocolate Feast DONE

--- ChocolateFeast/ChocolateFeast/Program.cs
+++ ChocolateFeast/ChocolateFeast/Program.cs
@@ -11,5 +11,21 @@ namespace ChocolateFeast
         static void Main(string[] args)
         {
         }
+
+        static int chocolateFeast(int n, int c, int m)
+        {
+            int res = 0;
+            int ch = n / c;
+            res += ch;
+            while (ch>=m)
+            {
+                res += ch / m;
+                int t = ch / m;
+                ch = ch % m;
+                ch += t;
+            }
+
+            return res;
+        }
     }
 }
