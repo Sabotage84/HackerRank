0151950fad0eb1fd919b06f8c36ec29cc8c4fb52
Sabotage84 <sabotage84@mail.ru>
2020-07-21 23:26:15 +0300

    LisasWorkbook DONE

--- LisasWorkbook/LisasWorkbook/Program.cs
+++ LisasWorkbook/LisasWorkbook/Program.cs
@@ -15,6 +15,23 @@ namespace LisasWorkbook
         static int workbook(int n, int k, int[] arr)
         {
             int res = 0;
+            int page = 1;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int curTask = 1;
+                while (curTask <= arr[i])
+                {
+                    if (curTask <= page && page <= curTask + k - 1)
+                    {
+                        if (page <= arr[i])
+                            res++;
+                    }
+                    page++;
+                    curTask += k;
+                }
+
+            }
+
 
             return res;
         }
