3e03cd7a428416e33fa2a8addcaf24a105704b40
Sabotage84 <sabotage84@mail.ru>
2020-08-07 21:01:42 +0300

    ADD gap map proj

---
+++ MapOfGAp/MapOfGAp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapOfGAp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+
+        static string[] cavityMap(string[] grid)
+        {
+            string[] res;
+
+
+            return res;
+        }
+
+    }
+}
