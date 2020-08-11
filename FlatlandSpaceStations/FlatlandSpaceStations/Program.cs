20f33117fb9122a4bb505e386dcc992e5a3bf031
Sabotage84 <sabotage84@mail.ru>
2020-08-11 23:56:48 +0300

    FlatlandSpaceStations DONE

--- FlatlandSpaceStations/FlatlandSpaceStations/Program.cs
+++ FlatlandSpaceStations/FlatlandSpaceStations/Program.cs
@@ -10,6 +10,30 @@ namespace FlatlandSpaceStations
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(flatlandSpaceStations(20, new int[] { 13, 1, 11, 10, 6 }));
+            Console.ReadKey();
         }
+
+        static int flatlandSpaceStations(int n, int[] c)
+        {
+            Array.Sort(c);
+            int res = 0;
+            int start = 0;
+            int last = n - 1;
+            
+            for (int i = 0; i < c.Length; i++)
+            {
+                if (c[i] - start > res)
+                    res = c[i] - start;
+                start = c[i];
+            }
+            if (last - start > res/2 && last-start>c[0])
+                return last - start;
+            if (c[0] > res / 2 && last - start < c[0])
+                return c[0];
+            return res/2;
+
+        }
+
     }
 }
