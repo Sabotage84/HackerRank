92bbadabcc04aaa673d63eee528945016584f467
Sabotage84 <sabotage84@mail.ru>
2020-08-13 00:27:22 +0300

    ADD Manasa and Stones

---
+++ ManasaandStones/ManasaandStones/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManasaandStones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static int[] stones(int n, int a, int b)
+        {
+
+
+        }
+    }
+}
