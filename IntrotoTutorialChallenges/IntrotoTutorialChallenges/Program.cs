f23c4a526332076c704a9f455785770504e66e3f
Sabotage84 <sabotage84@mail.ru>
2020-08-12 22:54:01 +0300

    Intro to Tutorial ChallengesDONE

--- IntrotoTutorialChallenges/IntrotoTutorialChallenges/Program.cs
+++ IntrotoTutorialChallenges/IntrotoTutorialChallenges/Program.cs
@@ -11,5 +11,32 @@ namespace IntrotoTutorialChallenges
         static void Main(string[] args)
         {
         }
+
+        static int introTutorial(int V, int[] arr)
+        {
+            int res = 0;
+            int left = 0;
+            int right = arr.Length - 1;
+            int med ;
+            while (true)
+            {
+                med = left+ (right - left) / 2;
+                if (arr[med] > V)
+                {
+                    right = med;
+                }
+                else if (arr[med] < V)
+                {
+                    left = med+1;
+                }
+                else
+                    return med;
+
+                
+
+            }
+
+            return res;
+        }
     }
 }
