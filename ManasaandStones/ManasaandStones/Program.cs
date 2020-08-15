8d97bb7ac2c4f9f0a38dcbbacb91f171a1b546af
Sabotage84 <sabotage84@mail.ru>
2020-08-15 22:14:38 +0300

    stones done

--- ManasaandStones/ManasaandStones/Program.cs
+++ ManasaandStones/ManasaandStones/Program.cs
@@ -10,12 +10,28 @@ namespace ManasaandStones
     {
         static void Main(string[] args)
         {
+            int[] t1 = stones(58,69,24);
+            for (int i = 0; i < t1.Length; i++)
+            {
+                Console.Write(t1[i] + " ");
+                
+            }
+Console.ReadKey();
+
         }
 
         static int[] stones(int n, int a, int b)
         {
+            int[] res = new int[n];
+            if (a == b)
+                return new int[] { a * (n - 1) };
+            for (int i = 0, j=n-1; i < n; i++,j--)
+            {
+                res[i] = a * j + i * b;
+            }
+            Array.Sort(res);
 
-
+            return res;
         }
     }
 }
