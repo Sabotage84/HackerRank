8f69ce2845296d010346a6b04036aafa9a478b30
Sabotage84 <sabotage84@mail.ru>
2020-08-15 21:44:33 +0300

    ADD static int[] stones(int n, int a, int b) {

---
+++ ManasaAndStones/ManasaAndStones/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManasaAndStones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static int[] stones(int n, int a, int b)
+        {
+            int[] res = new int[];
+
+
+            return res;
+        }
+    }
+}
