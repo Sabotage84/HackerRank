f499ac22942698fb8102a99f67e5b651fb261162
Sabotage84 <sabotage84@mail.ru>
2020-08-17 20:42:48 +0300

    ADD Happy Ladybugs

---
+++ HappyLadybugs/HappyLadybugs/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HappyLadybugs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static string happyLadybugs(string b)
+        {
+            string res = "";
+            return res;
+
+        }
+    }
+}
