3bd9ba2c88c074b53b529c2565cc9d301183ce74
Sabotage84 <sabotage84@mail.ru>
2020-08-24 23:25:07 +0300

    ADD Fair Rations

---
+++ FairRations/FairRations/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FairRations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static int fairRations(int[] B)
+        {
+            int res = 0;
+
+
+            return res;
+        }
+    }
+}
