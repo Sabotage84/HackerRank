481e741ec396a4b67da849fafc2eea5377ce904f
Sabotage84 <sabotage84@mail.ru>
2020-08-25 23:34:58 +0300

    Fair Rations done

--- FairRations/FairRations/Program.cs
+++ FairRations/FairRations/Program.cs
@@ -12,12 +12,31 @@ namespace FairRations
         {
         }
 
-        static int fairRations(int[] B)
+        static string fairRations(int[] B)
         {
+
             int res = 0;
+            if (B[0] % 2 != 0)
+            {
+                B[0]++; B[1]++;
+                res += 2;
+            }
+            for (int i = 1; i < B.Length; i++)
+            {
+                if (B[i] % 2 != 0)
+                {
+                    if (i == B.Length - 1)
+                    {
+                        return "NO";
+                    }
+                    B[i]++; B[i + 1]++;
+                    res += 2;
+                }
 
+            }
 
-            return res;
+            return res.ToString();
         }
+
     }
 }
