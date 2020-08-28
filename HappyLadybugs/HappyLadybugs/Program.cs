080e4459e3dc6e391dcb1430e6d4138bcaf99a7c
Sabotage84 <sabotage84@mail.ru>
2020-08-28 21:17:55 +0300

    Happy Ladybugs DONE

--- HappyLadybugs/HappyLadybugs/Program.cs
+++ HappyLadybugs/HappyLadybugs/Program.cs
@@ -10,11 +10,54 @@ namespace HappyLadybugs
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(happyLadybugs("X_Y__X"));
+            Console.ReadKey();
         }
 
         static string happyLadybugs(string b)
         {
-            string res = "";
+            string res = "NO";
+            bool alresdyAllHappy = true;
+            Dictionary<Char, int> dic = new Dictionary<char, int>();
+            for (int i = 0; i < b.Length; i++)
+            {
+                if (i > 0 && i < b.Length - 1)
+                {
+                    if (b[i] != '_' && (b[i] != b[i + 1] && b[i] != b[i - 1]))
+                        alresdyAllHappy = false;
+                }
+                else if (i==0)
+                {
+                    if (b[i] != '_' && b[i] != b[i + 1])
+                        alresdyAllHappy = false;
+                }
+                else
+                {
+                    if (b[i] != '_' && b[i] != b[i - 1])
+                        alresdyAllHappy = false;
+                }
+                if (dic.ContainsKey(b[i]))
+                    dic[b[i]]++;
+                else
+                    dic.Add(b[i], 1);
+            }
+
+            if (alresdyAllHappy)
+                return "YES";
+            else
+            {
+                foreach (var item in dic)
+                {
+                    if (item.Key != '_' && item.Value == 1)
+                        return "NO";
+                    
+                }
+                if (dic.ContainsKey('_'))
+                    return "YES";
+                
+
+            }
+
             return res;
 
         }
