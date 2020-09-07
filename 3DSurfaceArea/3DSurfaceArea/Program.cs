667767daccda2d635c625995a9c44411289fea4f
Sabotage84 <sabotage84@mail.ru>
2020-09-07 15:09:52 +0300

    3D Surface Area DONE

--- 3DSurfaceArea/3DSurfaceArea/Program.cs
+++ 3DSurfaceArea/3DSurfaceArea/Program.cs
@@ -16,7 +16,53 @@ namespace _3DSurfaceArea
         static int surfaceArea(int[][] A)
         {
             int res = 0;
+            for (int i = 0; i < A.Length; i++)
+            {
+                for (int j = 0; j < A[i].Length; j++)
+                {
+                    if(A[i][j]>0)
+                    {
+                        res += 2;
+                        if(j+1 < A[i].Length)
+                        {
+                            if (A[i][j] > A[i][j + 1])
+                                res += A[i][j] - A[i][j + 1];
+                        }
+                        else
+                        {
+                            res += A[i][j];
+                        }
+                        if(i+1<A.Length)
+                        {
+                            if (A[i][j] > A[i+1][j])
+                                res += A[i][j] - A[i+1][j];
+                        }
+                        else
+                        {
+                            res += A[i][j];
+                        }
+                        if (j-1>=0)
+                        {
+                            if (A[i][j] > A[i][j-1])
+                                res += A[i][j] - A[i][j-1];
+                        }
+                        else
+                        {
+                            res += A[i][j];
+                        }
+                        if(i-1>=0)
+                        {
+                            if (A[i][j] > A[i-1][j])
+                                res += A[i][j] - A[i-1][j];
+                        }
+                        else
+                        {
+                            res += A[i][j];
+                        }
 
+                    }
+                }
+            }
 
             return res;
         }
