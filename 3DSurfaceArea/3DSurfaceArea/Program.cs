a3a6cb6b1fba320cd13fae32e433094336b01a4f
Sabotage84 <sabotage84@mail.ru>
2020-09-07 14:48:37 +0300

    ADD 3D Surface Area

---
+++ 3DSurfaceArea/3DSurfaceArea/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3DSurfaceArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static int surfaceArea(int[][] A)
+        {
+            int res = 0;
+
+
+            return res;
+        }
+    }
+}
