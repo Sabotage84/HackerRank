910c3610dd13dba5d59816fa7fb4e35cd79260de
Sabotage84 <sabotage84@mail.ru>
2020-09-07 14:37:34 +0300

    StrangeCounter DONE

---
+++ StrangeCounter/StrangeCounter/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrangeCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static long strangeCounter(long t)
+        {
+            long t1 = 3;
+            long time = 0;
+            while (time+t1<t)
+            {
+                time += t1;
+                t1 *= 2;
+            }
+            return t1 - (t - time)+1;
+
+           
+        }
+    }
+}
