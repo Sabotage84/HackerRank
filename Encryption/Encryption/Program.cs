d08370d1dd28fae93120e417bb4476e10d8ea510
Sabotage84 <sabotage84@mail.ru>
2020-09-14 14:08:35 +0300

    ADD Encryption

---
+++ Encryption/Encryption/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encryption
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static string encryption(string s)
+        {
+            string res = "";
+            return res;
+        }
+    }
+}
