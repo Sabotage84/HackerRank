69a5655a4e5e1f56b0e9a15a103e0635a27b89be
Sabotage84 <sabotage84@mail.ru>
2020-09-21 14:31:04 +0300

    ADD OrganizingContainersOfBalls

---
+++ OrganizingContainersOfBalls/OrganizingContainersOfBalls/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrganizingContainersOfBalls
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static string organizingContainers(int[][] container)
+        {
+            string res = "";
+
+
+            return res;
+
+        }
+    }
+}
