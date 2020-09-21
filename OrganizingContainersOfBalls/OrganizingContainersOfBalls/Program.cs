938514b9d04c9aeaf67edfaede47526fcb810f6f
Sabotage84 <sabotage84@mail.ru>
2020-09-21 16:12:05 +0300

    OrganizingContainersOfBalls not work

--- OrganizingContainersOfBalls/OrganizingContainersOfBalls/Program.cs
+++ OrganizingContainersOfBalls/OrganizingContainersOfBalls/Program.cs
@@ -14,10 +14,28 @@ namespace OrganizingContainersOfBalls
 
         static string organizingContainers(int[][] container)
         {
-            string res = "";
-
-
-            return res;
+           
+            long l = 0;
+            bool flag = true;
+            long n = 0;
+            for (int i = 0; i < container.Length; i++)
+            {
+                for (int j = 0; j < container.Length; j++)
+                {
+                    l += container[i][j];
+                    l -= container[i][i];
+                    n += container[j][i];
+                    n -= container[i][i];
+                }
+                if (l - n != 0)
+                    flag = false;
+                l = 0;
+                n = 0;
+            }
+            if (flag)
+                return "Possible";
+            else
+                return "Impossible";
 
         }
     }
