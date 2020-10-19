27c776682cae3c58db7c9fb0ff1f061a028989aa
Sabotage84 <sabotage84@mail.ru>
2020-10-19 23:26:22 +0300

    Bigger is greater first test passed

--- BiggerIsGreater/BiggerIsGreater/Program.cs
+++ BiggerIsGreater/BiggerIsGreater/Program.cs
@@ -10,12 +10,46 @@ namespace BiggerIsGreater
     {
         static void Main(string[] args)
         {
+            biggerIsGreater("ab");
+            Console.ReadLine();
         }
         static string biggerIsGreater(string w)
         {
-            string res = "";
-            return res;
+            
+            int[] arr = new int[w.Length];
+            for (int i = 0; i < w.Length; i++)
+            {
+                arr[i] = w[i];
+            }
+            
 
+            for (int i = arr.Length-1; i >0; i--)
+            {
+                for (int j = i-1; j > -1; j--)
+                {
+                    if(arr[i]>arr[j])
+                    {
+                        int t = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = t;
+                        string s1 = "";
+                        for (int k = 0; k <= j; k++)
+                        {
+                            s1 += (char)arr[k];
+                        }
+                        string s2 = "";
+                        for (int k = j+1; k < arr.Length; k++)
+                        {
+                            s2 += (char)arr[k];
+                        }
+                        char[] CH = s2.ToCharArray();
+                        Array.Sort(CH);
+                        s2 = new string(CH);
+                        return s1 + s2;
+                    }
+                }
+            }
+        return "no answer";
         }
 
     }
