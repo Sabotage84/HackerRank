cb11a9ed89d6eb2b72858decf556886b17577e15
Sabotage84 <sabotage84@mail.ru>
2020-10-28 23:24:54 +0300

    Larrys Array done

--- LarryArray/LarryArray/Program.cs
+++ LarryArray/LarryArray/Program.cs
@@ -10,14 +10,68 @@ namespace LarryArray
     {
         static void Main(string[] args)
         {
+            int[] D = { 1, 6, 5, 2, 3, 4 };
+            Console.WriteLine(larrysArray(D));
+            Console.ReadKey();
+
         }
 
         static string larrysArray(int[] A)
         {
-            string res = "";
+            
+            for (int i = 0; i < A.Length-3; i++)
+            {
+                if ((i+1)!=A[i])
+                {
+                    for (int j = i+1; j < A.Length; j++)
+                    {
+                        if(A[j]==i+1)
+                        {
+                            int cur = j;
+                            while (cur!=i)
+                            {
+                                if(cur-i>=2)
+                                {
+                                    cur = ABC_CAB(A, cur);
+                                }
+                                if((cur-i)==1)
+                                {
+                                    cur = ABC_BCA(A, cur);
+                                }
+                            }
+                            break;
+                        }
+                    }
+                }
+            }
+            if (A[A.Length - 3] < A[A.Length - 2] && A[A.Length - 2] < A[A.Length - 1])
+                return "YES";
+            ABC_CAB(A, A.Length - 1);
+            if (A[A.Length - 3] < A[A.Length - 2] && A[A.Length - 2] < A[A.Length - 1])
+                return "YES";
+            ABC_CAB(A, A.Length - 1);
+            if (A[A.Length - 3] < A[A.Length - 2] && A[A.Length - 2] < A[A.Length - 1])
+                return "YES";
 
+            return "NO";
+        }
 
-            return res;
+        private static int ABC_CAB(int[] a, int cur)
+        {
+            int tmp = a[cur];
+            a[cur] = a[cur - 1];
+            a[cur - 1] = a[cur - 2];
+            a[cur - 2] = tmp;
+            return cur - 2;
+        }
+
+        private static int ABC_BCA(int[] a, int cur)
+        {
+            int tmp = a[cur];
+            a[cur] = a[cur + 1];
+            a[cur + 1] = a[cur - 1];
+            a[cur - 1] = tmp;
+            return cur - 1;
         }
 
 
