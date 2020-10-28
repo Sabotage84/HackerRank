97b277c5f11aa1d3089a638b8a244bf4dfa8f878
Sabotage84 <sabotage84@mail.ru>
2020-10-29 00:00:35 +0300

    ADD Grid search proj

---
+++ TheGridSearch/TheGridSearch/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheGridSearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        static string gridSearch(string[] G, string[] P)
+        {
+
+            return "NO";
+        }
+
+
+    }
+}
