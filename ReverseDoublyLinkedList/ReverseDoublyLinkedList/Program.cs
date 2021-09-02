4390d8fe15c391e5115ca209ddb95312687ae0e9
Sabotage84 <sabotage84@mail.ru>
2021-09-02 22:06:28 +0300

    Add Reverse a doubly linked list

---
+++ ReverseDoublyLinkedList/ReverseDoublyLinkedList/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseDoublyLinkedList
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+        }
+    }
+    class Result
+    {
+        /*
+     * Complete the 'reverse' function below.
+     *
+     * The function is expected to return an INTEGER_DOUBLY_LINKED_LIST.
+     * The function accepts INTEGER_DOUBLY_LINKED_LIST llist as parameter.
+     */
+
+        /*
+         * For your reference:
+         *
+         * DoublyLinkedListNode
+         * {
+         *     int data;
+         *     DoublyLinkedListNode next;
+         *     DoublyLinkedListNode prev;
+         * }
+         *
+         */
+        public static DoublyLinkedListNode reverse(DoublyLinkedListNode llist)
+        {
+
+        }
+    }
+}       
+    
