f24af9095c6a94120718b3aac675284a1a2d8d82
Sabotage84 <sabotage84@mail.ru>
2021-09-20 23:59:30 +0300

    Add 111

--- FindMergePointOfTwoLists/FindMergePointOfTwoLists/Program.cs
+++ FindMergePointOfTwoLists/FindMergePointOfTwoLists/Program.cs
@@ -8,6 +8,76 @@ namespace FindMergePointOfTwoLists
 {
     class Program
     {
+
+        class SinglyLinkedListNode
+        {
+            public int data;
+            public SinglyLinkedListNode next;
+
+            public SinglyLinkedListNode(int nodeData)
+            {
+                this.data = nodeData;
+                this.next = null;
+            }
+        }
+
+        class SinglyLinkedList
+        {
+            public SinglyLinkedListNode head;
+            public SinglyLinkedListNode tail;
+
+            public SinglyLinkedList()
+            {
+                this.head = null;
+                this.tail = null;
+            }
+
+            public void InsertNode(int nodeData)
+            {
+                SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+
+                if (this.head == null)
+                {
+                    this.head = node;
+                }
+                else
+                {
+                    this.tail.next = node;
+                }
+
+                this.tail = node;
+            }
+        }
+
+        static int findMergeNode(SinglyLinkedListNode head1, SinglyLinkedListNode head2)
+        {
+            while (true)
+            {
+                if (head1.data > head2.data)
+                {
+                    head2 = head2.next;
+                }
+                else if (head1.data < head2.data)
+                {
+                    head1 = head1.next;
+                }
+                else
+                {
+                    if (object.ReferenceEquals(head1, head2))
+                        return head1.data;
+                    else
+                    {
+                        if (head2.next.data > head1.next.data)
+                            head1 = head1.next;
+                        else
+                            head2 = head2.next;
+                    }
+                }
+            }
+
+            
+        }
+
         static void Main(string[] args)
         {
         }
